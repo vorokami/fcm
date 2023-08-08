ed5ebf9e5de8cbc481e12d3a53569b2bfc9cf704
Ivan Posazhennikov <ivanposazhennikov99@gmail.com>
2023-08-08 18:41:56 +0300

    fix: change fields of PushNotification

--- notification.go
+++ notification.go
@@ -1,13 +1,13 @@
 package fcm
 
 type PushNotification struct {
-	Title                        string
-	Body                         string
-	Category                     string
-	InAppImage                   string
+	Title                        string // title of push notification
+	Body                         string // text body of push notification
 	Tokens                       []string
 	URL                          string
 	ExternalURL                  string
-	AndroidPushNotificationImage string
-	Badge                        int
+	AndroidPushNotificationImage string // image URL
+	Badge                        int    // unread notification count to set on app icon
+	Sound                        string // name of sound of the push notification. This sound is set when building the application
+	ChannelID                    string
 }
